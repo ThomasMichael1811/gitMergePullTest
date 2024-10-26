daa20abb1a9aedca41786ecd34e96efe65360c58
Thomas Michael <thomas.michael@cloudogu.com>
2024-10-26 11:45:39 +0200

    Init springboot

---
+++ GitPullMergeTest/src/main/java/com/example/gitpullmergetest/GitPullMergeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpullmergetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPullMergeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPullMergeTestApplication.class, args);
+    }
+
+}
