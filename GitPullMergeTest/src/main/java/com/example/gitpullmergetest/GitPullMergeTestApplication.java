fee64ca47c0a846749d03638274538433c40a6f8
Thomas Michael <thomas.michael@cloudogu.com>
2024-10-26 11:50:15 +0200

    PA 3

--- GitPullMergeTest/src/main/java/com/example/gitpullmergetest/GitPullMergeTestApplication.java
+++ GitPullMergeTest/src/main/java/com/example/gitpullmergetest/GitPullMergeTestApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GitPullMergeTestApplication {
 
+    private String plan1= "emptpxy"
     public static void main(String[] args) {
         SpringApplication.run(GitPullMergeTestApplication.class, args);
     }
