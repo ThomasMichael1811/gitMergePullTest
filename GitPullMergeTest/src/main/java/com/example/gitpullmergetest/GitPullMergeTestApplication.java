39ca3b76a96988a905384f2432932b81741fabcf
Thomas Michael <thomas.michael@cloudogu.com>
2024-10-26 11:57:42 +0200

    PA 4

--- GitPullMergeTest/src/main/java/com/example/gitpullmergetest/GitPullMergeTestApplication.java
+++ GitPullMergeTest/src/main/java/com/example/gitpullmergetest/GitPullMergeTestApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitPullMergeTestApplication {
 
     private String plan1= "emptpxy"
+
+            private String after ="Diese Änderung ist zeitlich nach dem anderen Branch";
     public static void main(String[] args) {
         SpringApplication.run(GitPullMergeTestApplication.class, args);
     }
